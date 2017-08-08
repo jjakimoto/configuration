08c8f5e5d8e72fa1f026bd2808d8c8e003781b98
jjakimoto <f.j.akimoto@gmail.com>
2017-08-08 21:57:21 +0000

    adding others

--- python_env.sh
+++ python_env.sh
@@ -3,6 +3,21 @@ sudo apt-get install python3-dev
 sudo apt-get install enchant
 sudo apt-get install libcupti-dev
 conda install libgcc
+cd ~/Downloads
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -zxvf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+sudo make install
+
+# install Anaconda
+curl -O https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64h
+bash Anaconda3-4.3.1-Linux-x86_64.sh
+cd ~
+source .bashrc
+jupyter notebook --generate-config
+
 
 # setting for git
 sudo apt-get install git-flow
@@ -11,7 +26,7 @@ git config --global user.name "jjakimoto"
 git config --global credential.helper store
 
 # Install only from pip
-pip isntall tensorflow-gpu
+pip install tensorflow-gpu
 pip install GPy
 pip install nltk
 pip install xgboost
@@ -30,9 +45,14 @@ pip install pyenchant
 
 # Make directry in case
 
-mkdir ~/Downloads
-cd ~/Downloads
+mkdir ~/library
+cd ~/library
 git clone https://github.com/GPflow/GPflow.git
-cd ~/Downloads/GPflow 
-~/anaconda3/bin/python setup.py develop
+cd GPflow 
+python setup.py develop
+echo "PATH='~/library/GPflow:$PATH'" >> ~/.bashrc
+source ~/.bashrc
 
+mv -f ~/Downloads/ta-lib ~/library/
+rm -rf ~/Downloads/Anaconda3-4.3.1-Linux-x86_64.sh
+rm -rf ~/Downloads/ta-lib-0.4.0-src.tar.gz
