5762512af9338f4b9674553d1654a394538d06a6
jjakimoto <f.j.akimoto@gmail.com>
2017-08-08 17:55:25 +0000

    add git configuration

--- python_env.sh
+++ python_env.sh
@@ -2,17 +2,28 @@
 sudo apt-get install python3-dev
 sudo apt-get install enchant
 sudo apt-get install libcupti-dev
+conda install libgcc
+
+# setting for git
+sudo apt-get install git-flow
+git config --global user.email "f.j.akimoto@gmail.com"
+git config --global user.name "jjakimoto"
+git config --global credential.helper store
 
 # Install only from pip
 pip isntall tensorflow-gpu
 pip install GPy
 pip install nltk
 pip install xgboost
+pip install TA-Lib
+pip install tqdm
+pip install gensim
+pip install google-compute-engine
 
 # Small Thried Party
 pip install pykalman
-pip install hmmlearn
-pip install skrvmi
+pip install hmmlearn 
+pip install https://github.com/JamesRitchie/scikit-rvm/archive/master.zip
 
 # From source
 pip install pyenchant
@@ -20,8 +31,8 @@ pip install pyenchant
 # Make directry in case
 
 mkdir ~/Downloads
- cd ~/Downloads
+cd ~/Downloads
 git clone https://github.com/GPflow/GPflow.git
-cd GPflow 
-python GPflow develop
+cd ~/Downloads/GPflow 
+~/anaconda3/bin/python setup.py develop
 
